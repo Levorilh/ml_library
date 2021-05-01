a3080f2294effd339c9b58e155fd6b076953ccb7
Horville <59647345+Horville@users.noreply.github.com>
2021-05-01 16:16:09 +0200

    Update MLP_model.cpp

--- MLP_model.cpp
+++ MLP_model.cpp
@@ -11,9 +11,11 @@ using namespace std;
 
 float *create_mlp_model(int);
 float *forward_pass(float sample_inputs, bool is_classification);
-float train_stochastic_gradient_descent(float flattened_dataset_inputs,
+float *train_stochastic_gradient_descent(float flattened_dataset_inputs,
                                         float flattened_dataset_expected_outputs,
-                                        bool is_classifi);
+                                        bool is_classification,
+                                        float alpha = 0.001,
+                                        int iterations_count = 10000);
 
 int main() {
 
