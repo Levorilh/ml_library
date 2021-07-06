107950bfd9b347dd40a6359b52dfc5f7f4e09b2d
Horville <59647345+Horville@users.noreply.github.com>
2021-07-06 22:30:03 +0200

    Update create.cpp

--- src/svm/create.cpp
+++ src/svm/create.cpp
@@ -46,12 +46,37 @@ void create_svm_model(float* flattened_dataset_inputs, int input_dim, int sample
         negatifs(0, i) = -1;
     }
 
-    //minimiser
-    MatrixXd min = 1/2 * alphas.transpose() * BigMatrix * alphas + negatifs * alphas;
-    MatrixXd Yt = Y.transpose() * alphas;
-    cout << "min : " << min(0,0) <<endl;
-    cout << "Yt.aplha : " << Yt(0,0) <<endl;
+    //TODO pas encore la fonction pour minimiser alpha
+    float min = (1/2 * alphas.transpose() * BigMatrix * alphas + negatifs * alphas)(0,0);
+    float Yt = (Y.transpose() * alphas)(0,0);
+    cout << "min : " << min << endl;
+    cout << "Yt.aplha : " << Yt << endl;
     for(int i = 0; i< samples_count; i++){
-        cout << "aplha : " << alphas(i,0) <<endl;
+        cout << "aplha : " << alphas(i,0) << endl;
+    }
+
+    MatrixXd TmpW(1,input_dim);
+    for(int i =1; i < samples_count; i++){
+        MatrixXd Xi(1,input_dim);
+        for(int y=0; y<input_dim; y++){
+            Xi(0,y)=X(i,y);
+        }
+        TmpW += alphas(i,0) * Y(i,0) * Xi;
+    }
+
+    //TODO svPos reste a calculer
+    int svPos = 0;
+
+    float sumWX = 0;
+    for(int i =0; i< input_dim; i++){
+        sumWX += TmpW(0,i) * X(svPos, i) ;
+    }
+
+    float W0 = 1/Y(svPos,0) - sumWX;
+
+    MatrixXd W(1,input_dim + 1);
+    W(0,0) = W0;
+    for(int i = 0; i < samples_count; i++){
+        W(0, i+1) = TmpW(0, i);
     }
 }
