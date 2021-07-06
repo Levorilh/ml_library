2ad862d65321693516a692868f51632357cf074e
Thomas Tresgots <ttresgots@gmail.com>
2021-07-06 19:05:06 +0200

    use fopen instead of fopen_s

--- src/linear/save.cpp
+++ src/linear/save.cpp
@@ -3,6 +3,7 @@
 
 DLLEXPORT void save_linear_model(float* model, int input_dim, char *path){
     string model_to_string = "";
+
     for (int i =0; i<input_dim + 1; i++){// +1 pour le biai
         if(i>0){
             model_to_string = model_to_string + ";";
@@ -10,8 +11,7 @@ DLLEXPORT void save_linear_model(float* model, int input_dim, char *path){
         model_to_string = model_to_string + std::to_string(model[i]);
     }
 
-    FILE *save_file;
-    fopen_s(&save_file, path , "w");
+    FILE *save_file =  fopen(path , "w");
     fwrite(model_to_string.c_str() , sizeof(char) , model_to_string.size() ,save_file);
     fclose(save_file);
 }
