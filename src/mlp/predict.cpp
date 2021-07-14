d2586bf0684717a01876b0a8ad95f71c2e41fda3
Thomas Tresgots <ttresgots@gmail.com>
2021-07-14 19:32:31 +0200

    fix same bug for predict

--- src/mlp/predict.cpp
+++ src/mlp/predict.cpp
@@ -3,7 +3,7 @@
 float* predict_mlp_model_regression(MLP * mlp, float * sample_input){
     forward_pass(mlp,sample_input,false);
 
-    auto * result = (float *)malloc(sizeof(float) * mlp->d_length - 1);
+    auto result = new float[mlp->d[mlp->d_length - 1]];
     for(int i = 1; i < (mlp->d[mlp->d_length - 1] + 1); i++){
         result[i - 1] = mlp->X[mlp->d_length - 1][i];
     }
